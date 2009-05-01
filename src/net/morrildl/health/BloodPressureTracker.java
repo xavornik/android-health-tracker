61eba0cf38794a66cc25fba1bd55e0b2585a7be2
Dan Morrill <morrildl@gmail.com>
2009-05-01 02:04:10 +0100

    Worked out a more efficient way to do the layouts. BP converted over; others are next. Also updating TODO.

--- src/net/morrildl/health/BloodPressureTracker.java
+++ src/net/morrildl/health/BloodPressureTracker.java
@@ -17,6 +17,7 @@ package net.morrildl.health;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -27,6 +28,7 @@ public class BloodPressureTracker extends DataCollectorBaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.bp_tracker);
 
 		final EditText systolic = (EditText) findViewById(R.id.systolic);
